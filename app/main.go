89d3de454ad29e32e8459c6d6ac07c0bbe433341
ravielze <ravielz20@gmail.com>
2021-03-06 03:07:55 +0700

    Add environment variables loader

--- app/main.go
+++ app/main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println("Starting server...")
 	r := gin.Default()
 	r.Run()
