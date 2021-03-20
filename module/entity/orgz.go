7bc563f184691d863401f92b3a87892af1732fb9
MazayaCode <msulthanmazaya@gmail.com>
2021-03-21 01:24:09 +0700

    Add organization repo and usecase
    masih ada handling error yang belom di add

--- module/entity/orgz.go
+++ module/entity/orgz.go
@@ -1,6 +1,8 @@
 package entity
 
-import "github.com/paradewisudaitb/Backend/common/domain"
+import (
+	"github.com/paradewisudaitb/Backend/common/domain"
+)
 
 type Orgz struct {
 	domain.EntityBase
@@ -10,4 +12,41 @@ type Orgz struct {
 	ApresiasiPoster  string `gorm:"type:VARCHAR(255);"`
 	ApresiasiTulisan string `gorm:"type:text;"`
 	ApresiasiVideo   string `gorm:"type:VARCHAR(255);"`
+}
+
+type CreateOrgzSerializer struct {
+	Name             string  `json:"name"`
+	Category         string  `json:"category"`
+	Logo             string  `json:"logo"`
+	ApresiasiPoster  *string `json:"apresiasi_poster"`
+	ApresiasiTulisan *string `json:"apresiasi_tulisan"`
+	ApresiasiVideo   *string `json:"apresiasi_video"`
+}
+
+type UpdateOrgzSerializer struct {
+	IdOrgz           string  `json:"id_organization"`
+	Name             *string `json:"name"`
+	Category         *string `json:"category"`
+	Logo             *string `json:"logo"`
+	ApresiasiPoster  *string `json:"apresiasi_poster"`
+	ApresiasiTulisan *string `json:"apresiasi_tulisan"`
+	ApresiasiVideo   *string `json:"apresiasi_video"`
+}
+
+type DeleteOrgzSerializer struct {
+	IdOrgz string `json:"id_organization"`
+}
+
+type OrgzUseCase interface {
+	CreateOrgz(item CreateOrgzSerializer) error
+	DeleteOrgz(item DeleteOrgzSerializer) error
+	UpdateOrgz(item UpdateOrgzSerializer) error
+	GetOrgz(idOrgz string) (Orgz, error)
+}
+
+type OrgzRepository interface {
+	GetOne(id_orgz string) (Orgz, error)
+	AddOne(name, category, logo, apresiasi_poster, apresiasi_tulisan, apresiasi_video string)
+	UpdateOne(id_organization string, name, category, logo, apresiasi_poster, apresiasi_tulisan, apresiasi_video *string) error
+	DeleteOne(id_orgz string) error
 }
---
+++ module/usecase/orgz.go
@@ -0,0 +1,45 @@
+package content
+
+import (
+	"github.com/paradewisudaitb/Backend/module/entity"
+	"github.com/paradewisudaitb/Backend/module/repository"
+)
+
+type OrgzUseCase struct {
+	orgzrepo repository.OrgzRepository
+}
+
+func NewOrgzUsecase(a repository.OrgzRepository) entity.OrgzUseCase {
+	return &OrgzUseCase{
+		orgzrepo: a,
+	}
+}
+
+// gak tau mau handling error kalau apa
+func (a *OrgzUseCase) CreateOrgz(item entity.CreateOrgzSerializer) error {
+	a.orgzrepo.AddOne(item.Name, item.Category, item.Logo, *item.ApresiasiPoster, *item.ApresiasiTulisan, *item.ApresiasiVideo)
+	return nil
+}
+
+func (a *OrgzUseCase) DeleteOrgz(item entity.DeleteOrgzSerializer) error {
+	err := a.orgzrepo.DeleteOne(item.IdOrgz)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *OrgzUseCase) UpdateOrgz(item entity.UpdateOrgzSerializer) error {
+	err := a.orgzrepo.UpdateOne(item.IdOrgz, item.Name, item.Category, item.Logo, item.ApresiasiPoster, item.ApresiasiTulisan, item.ApresiasiVideo)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (a *OrgzUseCase) GetOrgz(idOrgz string) (entity.Orgz, error) {
+	orgz, err := a.orgzrepo.GetOne(idOrgz)
+	if err != nil {
+		return orgz, err
+	}
+	return orgz, nil
+}
