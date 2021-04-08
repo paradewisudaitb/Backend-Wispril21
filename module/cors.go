7f8a304e2cd30734cc19d6510e39add9ca792580
ravielze <ravielz20@gmail.com>
2021-04-09 00:23:35 +0700

    Allow all origins

--- module/cors.go
+++ module/cors.go
@@ -10,16 +10,12 @@ import (
 func NewCORSModule(g *gin.Engine, devmode bool) {
 
 	corsConfig := cors.Config{
-		AllowOrigins:     []string{"https://wisprilitb.com", "https://staging.wisprilitb.com"},
+		AllowAllOrigins:  true,
 		AllowMethods:     []string{"PUT", "GET", "POST", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Access-Control-Allow-Origin"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           1 * time.Hour,
-	}
-
-	if devmode {
-		corsConfig.AllowOrigins = append(corsConfig.AllowOrigins, "*")
 	}
 	g.Use(cors.New(corsConfig))
 }
