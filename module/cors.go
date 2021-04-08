2eeabe19c3b0b1e41ba9e61436ebf9754a341156
ravielze <ravielz20@gmail.com>
2021-04-08 22:44:24 +0700

    Add cors header

--- module/cors.go
+++ module/cors.go
@@ -12,7 +12,7 @@ func NewCORSModule(g *gin.Engine, devmode bool) {
 	corsConfig := cors.Config{
 		AllowOrigins:     []string{"https://wisprilitb.com", "https://staging.wisprilitb.com"},
 		AllowMethods:     []string{"PUT", "GET", "POST", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Access-Control-Allow-Origin"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           1 * time.Hour,
